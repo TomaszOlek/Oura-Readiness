5e6bde04b58cc49e1f669b5125f25ed06cf8f8ad
Tomasz Olek <tomasz.olek.dev@gmail.com>
2022-09-22 11:47:35 +0200

    t

--- packages/starter-widget/src/Content.js
+++ packages/starter-widget/src/Content.js
@@ -134,7 +134,7 @@ const DryRun = (props) => {
     //process the asyncData
     if (stage === "dev") {
       processAsyncData(asyncFalseData);
-      processData(result.data.getDataObject.content[0]); //I had to add the [0] here
+      processData(result.data.getDataObject.content);
     }
   }, []);
 
